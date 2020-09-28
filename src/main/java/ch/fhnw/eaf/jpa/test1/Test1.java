47f185240f521765cf6265baf39abc587f906d52
Janick <janick.huerzeler@students.fhnw.ch>
2020-09-28 10:59:04 +0200

    Added parts of exercise 1)

--- src/main/java/ch/fhnw/eaf/jpa/test1/Test1.java
+++ src/main/java/ch/fhnw/eaf/jpa/test1/Test1.java
@@ -38,6 +38,45 @@ public class Test1 implements CommandLineRunner {
 
 		em.close();
 		System.out.println("done");
+
+		part1();
+
+		part2();
+
 		System.exit(0);
+	}
+
+	/**
+	 * Persistenzkontext leeren
+	 */
+	private void part1() {
+		EntityManager em = emf.createEntityManager();
+
+		Customer c1 = em.find(Customer.class, 1);
+		em.clear();
+		Customer c2 = em.find(Customer.class, 1);
+
+		System.out.println(c1);
+		System.out.println(c2);
+
+		System.out.println(c1 == c2);
+
+		em.close();
+		System.out.println("done part1");
+	}
+
+	/**
+	 * Unterschiedliche Entity-Manager
+	 */
+	private void part2() {
+		Customer c1 = emf.createEntityManager().find(Customer.class, 1);
+		Customer c2 = emf.createEntityManager().find(Customer.class, 1);
+
+		System.out.println(c1);
+		System.out.println(c2);
+
+		System.out.println(c1 == c2);
+
+		System.out.println("done part2");
 	}
 }
